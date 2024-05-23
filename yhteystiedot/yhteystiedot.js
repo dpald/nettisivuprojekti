eb3eacb06117f3347e838181d6c3f34ca495167e
Toni Eskola <toni.eskola992@gmail.com>
2024-05-23 15:42:14 +0300

    yhteystietosivun muutokset

---
+++ yhteystiedot/yhteystiedot.js
@@ -0,0 +1,4 @@
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
