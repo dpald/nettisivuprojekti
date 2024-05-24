cef381f0c0f5ce112b98a9fc221074683848dd8a
Daniel Paldanius <daniel.paldanius.23@edu.luksia.fi>
2024-05-24 08:03:51 +0300

    hienoa!

---
+++ etusivu/etusivu.js
@@ -0,0 +1,37 @@
+
+const slider = document.querySelector('.slider');
+const navigaatio = document.querySelector('.navigaatio');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+   
+var direction = 1;
+
+prev.addEventListener('click', function() {
+    if (direction === -1) {
+        slider.appendChild(slider.firstElementChild);
+        direction = 1;
+    }
+    navigaatio.style.justifyContent = 'flex-end';
+    slider.style.transform = 'translate(14.28%)';
+});
+
+next.addEventListener('click', function() {
+    direction = -1;
+    navigaatio.style.justifyContent = 'flex-start';
+    slider.style.transform = 'translate(-14.28%)';
+});
+
+slider.addEventListener('transitionend', function() {
+    if (direction === -1) {
+        slider.appendChild(slider.firstElementChild);
+    } else if (direction === 1) {
+        slider.prepend(slider.lastElementChild);
+    }
+
+    slider.style.transition = 'none';
+    slider.style.transform = 'translate(0)';
+    setTimeout(function(){
+        slider.style.transition = 'all 0.5s';
+    });
+}); 
